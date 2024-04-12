9c111395bd28aeda2c8911ca376c0084fa628da7
Devin-C285 <devin.chamberlain@maine.edu>
2024-04-12 09:56:32 -0400

    RedBlackTree added
    Added RedBlackTree class

---
+++ src/accidentpack/RedBlackTree.java
@@ -0,0 +1,242 @@
+package accidentpack;
+
+/**
+ * @author Devin C 
+ * This class implements a Red-Black search tree for report objects.
+ */
+public class RedBlackTree {
+    // Define colors for Red-Black tree
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    // NODE structure
+    class Node {
+        report data;
+        boolean color;
+        Node left;
+        Node right;
+        Node parent;
+
+        // Constructor for Node
+        public Node(report data) {
+            this.data = data;
+            this.color = RED; // New nodes are always red
+            this.left = null;
+            this.right = null;
+            this.parent = null;
+        }
+    }
+
+    private Node root;
+
+    // Constructor
+    public RedBlackTree() {
+        root = null;
+    }
+
+    // Inserts a node with given value into the tree
+    public void add(report key) {
+        if (root == null) {
+            root = new Node(key);
+            root.color = BLACK; // Root is always black
+        } else {
+            root = insert(root, key);
+            root.color = BLACK; // Ensure root remains black
+        }
+    }
+
+    // BST insertion and balancing for Red-Black tree
+    private Node insert(Node root, report key) {
+        if (root == null)
+            return new Node(key);
+
+        if (key.compareTo(root.data) < 0) {
+            root.left = insert(root.left, key);
+            root.left.parent = root;
+        } else if (key.compareTo(root.data) > 0) {
+            root.right = insert(root.right, key);
+            root.right.parent = root;
+        }
+
+        // Balance the tree after insertion
+        return balance(root);
+    }
+
+    // Balance the tree after insertion
+    private Node balance(Node root) {
+        if (isRed(root.right))
+            root = rotateLeft(root);
+        if (isRed(root.left) && isRed(root.left.left))
+            root = rotateRight(root);
+        if (isRed(root.left) && isRed(root.right))
+            flipColors(root);
+
+        return root;
+    }
+
+    // Checks if node is red (or null is considered black)
+    private boolean isRed(Node node) {
+        if (node == null)
+            return false;
+        return node.color == RED;
+    }
+
+    // Performs left rotation
+    private Node rotateLeft(Node x) {
+        Node y = x.right;
+        x.right = y.left;
+        if (y.left != null)
+            y.left.parent = x;
+        y.left = x;
+        y.color = x.color;
+        x.color = RED;
+        return y;
+    }
+
+    // Performs right rotation
+    private Node rotateRight(Node y) {
+        Node x = y.left;
+        y.left = x.right;
+        if (x.right != null)
+            x.right.parent = y;
+        x.right = y;
+        x.color = y.color;
+        y.color = RED;
+        return x;
+    }
+
+    // Flip colors of a node and its children
+    private void flipColors(Node node) {
+        node.color = !node.color;
+        node.left.color = !node.left.color;
+        node.right.color = !node.right.color;
+    }
+
+    // Searches for a node with given value in the tree
+    public int search(report key) {
+        if (findNode(root, key) == null) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    // Finds the node with given value
+    private Node findNode(Node root, report key) {
+        if (root == null || key.compareTo(root.data) == 0)
+            return root;
+        if (key.compareTo(root.data) < 0)
+            return findNode(root.left, key);
+        else
+            return findNode(root.right, key);
+    }
+
+    // Deletes a node with given value from the tree
+    public void delete(report key) {
+        if (findNode(root, key) != null) {
+            root = remove(root, key);
+            System.out.println("Deletion successful ");
+        } else
+            System.out.println("No node with entered value found in tree");
+    }
+
+    // BST deletion and balancing for Red-Black tree
+    private Node remove(Node root, report key) {
+        if (key.compareTo(root.data) < 0) {
+            if (!isRed(root.left) && !isRed(root.left.left))
+                root = moveRedLeft(root);
+            root.left = remove(root.left, key);
+        } else {
+            if (isRed(root.left))
+                root = rotateRight(root);
+            if (key.compareTo(root.data) == 0 && root.right == null)
+                return null;
+            if (!isRed(root.right) && !isRed(root.right.left))
+                root = moveRedRight(root);
+            if (key.compareTo(root.data) == 0) {
+                Node x = min(root.right);
+                root.data = x.data;
+                root.right = deleteMin(root.right);
+            } else
+                root.right = remove(root.right, key);
+        }
+        return balance(root);
+    }
+
+    // Moves a red node to the left
+    private Node moveRedLeft(Node node) {
+        flipColors(node);
+        if (isRed(node.right.left)) {
+            node.right = rotateRight(node.right);
+            node = rotateLeft(node);
+            flipColors(node);
+        }
+        return node;
+    }
+
+    // Moves a red node to the right
+    private Node moveRedRight(Node node) {
+        flipColors(node);
+        if (isRed(node.left.left)) {
+            node = rotateRight(node);
+            flipColors(node);
+        }
+        return node;
+    }
+
+    // Deletes the minimum node
+    private Node deleteMin(Node node) {
+        if (node.left == null)
+            return null;
+        if (!isRed(node.left) && !isRed(node.left.left))
+            node = moveRedLeft(node);
+        node.left = deleteMin(node.left);
+        return balance(node);
+    }
+
+    // Finds the minimum node
+    private Node min(Node node) {
+        while (node.left != null)
+            node = node.left;
+        return node;
+    }
+
+    // Performs in-order traversal of the tree
+    void inOrder(Node root) {
+        if (root == null) {
+            System.out.println("No nodes in the tree");
+            return;
+        }
+        if (root.left != null)
+            inOrder(root.left);
+        System.out.print(root.data.getID() + " ");
+        if (root.right != null)
+            inOrder(root.right);
+    }
+
+    // Performs pre-order traversal of the tree
+    void preOrder(Node root) {
+        if (root == null) {
+            System.out.println("No nodes in the tree");
+            return;
+        }
+        System.out.print(root.data.getID() + " ");
+        if (root.left != null)
+            preOrder(root.left);
+        if (root.right != null)
+            preOrder(root.right);
+    }
+
+    // Performs post-order traversal of the tree
+    void postOrder(Node key) {
+        if (key == null) {
+            System.out.println("No nodes in the tree");
+            return;
+        }
+        if (key.left != null)
+            postOrder(key.left);
+        if (key.right != null)
+            postOrder(key.right);
+        System.out.print(key.data.getID() + " ");
+    }
+}
